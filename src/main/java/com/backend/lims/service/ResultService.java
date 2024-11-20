7075f81e5db0b6c87af45ec73b6518b433bec38b
MD-4Good0 <agudo.mdn@gmail.com>
2024-11-20 14:31:59 +0800

    contact number

---
+++ src/main/java/com/backend/lims/service/ResultService.java
@@ -0,0 +1,185 @@
+package com.backend.lims.service;
+
+import com.backend.lims.dto.ChemElisaTestDTO;
+import com.backend.lims.dto.ChemMicrobialTestDTO;
+import com.backend.lims.dto.MicrobioTestDTO;
+import com.backend.lims.dto.MolBioTestDTO;
+import com.backend.lims.model.*;
+import com.backend.lims.repository.*;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ResultService {
+    private final ResultRepository resultRepository;
+    private final ChemMicrobialRepository chemMicrobialRepository;
+    private final ChemElisaRepository chemElisaRepository;
+    private final MicrobioRepository microbioRepository;
+    private final MolBioRepository molBioRepository;
+    private final RequestRepository requestRepository;
+    private final SampleRepository sampleRepository;
+
+    @Autowired
+    public ResultService(ResultRepository resultRepository, ChemMicrobialRepository chemMicrobialRepository, MicrobioRepository microbioRepository, MolBioRepository molBioRepository, RequestRepository requestRepository, SampleRepository sampleRepository, ChemElisaRepository chemElisaRepository) {
+        this.resultRepository = resultRepository;
+        this.chemMicrobialRepository = chemMicrobialRepository;
+        this.microbioRepository = microbioRepository;
+        this.molBioRepository = molBioRepository;
+        this.requestRepository = requestRepository;
+        this.sampleRepository = sampleRepository;
+        this.chemElisaRepository = chemElisaRepository;
+    }
+
+    @Transactional
+    public Result createResult(Result result, Long requestId) {
+        // Save the new Result entity
+
+        List<Long> sampleIds = sampleRepository.findSampleIdsByRequestId(requestId);
+        result.setRequestId(requestId);
+        Result savedResult = resultRepository.save(result);
+        Request request = requestRepository.findByRequestId(requestId);
+
+        if (request.getMicrobial()) {
+            List<ChemMicrobialTestResults> chemMicrobialTestResultsList = new ArrayList<>();
+            for (Long sampleId : sampleIds) {
+                ChemMicrobialTestResults chemTestResult = new ChemMicrobialTestResults();
+                chemTestResult.setSampleId(sampleId);
+
+                chemMicrobialRepository.save(chemTestResult);
+                chemMicrobialTestResultsList.add(chemTestResult);
+            }
+
+            // Attach chemTestResultsList to savedResult and return
+            savedResult.setChemMicrobialTestResults(chemMicrobialTestResultsList);
+        }
+
+        if (request.getElisa()) {
+            List<ChemElisaTestResults> chemElisaTestResultsList = new ArrayList<>();
+            for (Long sampleId : sampleIds) {
+                ChemElisaTestResults chemTestResult = new ChemElisaTestResults();
+                chemTestResult.setSampleId(sampleId);
+
+                chemElisaRepository.save(chemTestResult);
+                chemElisaTestResultsList.add(chemTestResult);
+            }
+
+            // Attach chemTestResultsList to savedResult and return
+            savedResult.setChemElisaTestResults(chemElisaTestResultsList);
+        }
+
+        if (request.getMolBio()) {
+            List<MolBioTestResults> molBioTestResultsList = new ArrayList<>();
+            for (Long sampleId : sampleIds) {
+                MolBioTestResults molBioTestResult = new MolBioTestResults();
+                molBioTestResult.setSampleId(sampleId);
+
+                molBioRepository.save(molBioTestResult);
+                molBioTestResultsList.add(molBioTestResult);
+            }
+
+            // Attach chemTestResultsList to savedResult and return
+            savedResult.setMolBioTestResults(molBioTestResultsList);
+        }
+
+        if (request.getMicrobio()) {
+            List<MicrobioTestResults> microbioTestResultsList = new ArrayList<>();
+            for (Long sampleId : sampleIds) {
+                MicrobioTestResults microbioTestResult = new MicrobioTestResults();
+                microbioTestResult.setSampleId(sampleId);
+
+                microbioRepository.save(microbioTestResult);
+                microbioTestResultsList.add(microbioTestResult);
+            }
+
+            // Attach MicroBioTestResultsList to savedResult and return
+            savedResult.setMicrobioTestResults(microbioTestResultsList);
+        }
+
+        // Populate ChemTestResults with sampleIds and associate with the saved Result
+
+        return resultRepository.save(savedResult);
+    }
+
+    // Function 2: Update ChemTestResults by sampleId
+    public ChemMicrobialTestResults updateChemMicrobialTestResultData(Long sampleId, ChemMicrobialTestDTO chemMicrobialTestDTO) {
+        // Find the ChemTestResults by sampleId
+        ChemMicrobialTestResults chemTestResult = chemMicrobialRepository.findBySampleId(sampleId)
+                .orElseThrow(() -> new RuntimeException("ChemTestResult not found for sampleId: " + sampleId));
+
+        // Update fields
+        chemTestResult.setBetaLactams(chemMicrobialTestDTO.getBetaLactams());
+        chemTestResult.setTetracyclines(chemMicrobialTestDTO.getTetracyclines());
+        chemTestResult.setSulfonamides(chemMicrobialTestDTO.getSulfonamides());
+        chemTestResult.setAminoglycosides(chemMicrobialTestDTO.getAminoglycosides());
+        chemTestResult.setMacrolides(chemMicrobialTestDTO.getMacrolides());
+        chemTestResult.setQuinolones(chemMicrobialTestDTO.getQuinolones());
+
+        // Save updated ChemTestResult
+        return chemMicrobialRepository.save(chemTestResult);
+    }
+
+    public ChemElisaTestResults updateChemElisaTestResultData(Long sampleId, ChemElisaTestDTO chemElisaTestDTO) {
+        // Find the ChemTestResults by sampleId
+        ChemElisaTestResults chemTestResult = chemElisaRepository.findBySampleId(sampleId)
+                .orElseThrow(() -> new RuntimeException("ChemTestResult not found for sampleId: " + sampleId));
+
+        // Update fields
+        chemTestResult.setChloramphenicol(chemElisaTestDTO.getChloramphenicol());
+        chemTestResult.setNitrofuranAoz(chemElisaTestDTO.getNitrofuranAoz());
+        chemTestResult.setBeta_agonists(chemElisaTestDTO.getBeta_agonists());
+        chemTestResult.setCorticosteroids(chemElisaTestDTO.getCorticosteroids());
+        chemTestResult.setOlaquindox(chemElisaTestDTO.getOlaquindox());
+        chemTestResult.setNitrufuranAmoz(chemElisaTestDTO.getNitrufuranAmoz());
+        chemTestResult.setStilbenes(chemElisaTestDTO.getStilbenes());
+        chemTestResult.setRactopamine(chemElisaTestDTO.getRactopamine());
+
+        // Save updated ChemTestResult
+        return chemElisaRepository.save(chemTestResult);
+    }
+
+    @Transactional
+    public MicrobioTestResults updateMicrobioTestResultData(Long sampleId, MicrobioTestDTO microbioTestDTO) {
+        // Find the MicrobioTestResults by sampleId
+        MicrobioTestResults microbioTestResults = microbioRepository.findBySampleId(sampleId)
+                .orElseThrow(() -> new RuntimeException("MicrobioTestResult not found for sampleId: " + sampleId));
+
+        // Update fields
+        microbioTestResults.setStandardPlateCount(microbioTestDTO.getStandardPlateCount());
+        microbioTestResults.setStaphylococcusAureus(microbioTestDTO.getStaphylococcusAureus());
+        microbioTestResults.setSalmonellaSp(microbioTestDTO.getSalmonellaSp());
+        microbioTestResults.setCampylobacter(microbioTestDTO.getCampylobacter());
+        microbioTestResults.setCultureAndSensitivityTest(microbioTestDTO.getCultureAndSensitivityTest());
+        microbioTestResults.setColiformCount(microbioTestDTO.getColiformCount());
+        microbioTestResults.seteColi(microbioTestDTO.geteColi());
+        microbioTestResults.seteColiAndeColi0O157(microbioTestDTO.geteColiAndeColi0O157());
+        microbioTestResults.setYeastAndMolds(microbioTestDTO.getYeastAndMolds());
+
+        // Save updated MicrobioTestResult
+        return microbioRepository.save(microbioTestResults);
+    }
+
+    @Transactional
+    public MolBioTestResults updateMolBioTestResultData(Long sampleId, MolBioTestDTO molBioTestDTO) {
+        // Find the MolBioTestResults by sampleId
+        MolBioTestResults molBioTestResult = molBioRepository.findBySampleId(sampleId)
+                .orElseThrow(() -> new RuntimeException("MolBioTestResult not found for sampleId: " + sampleId));
+
+        // Update fields
+        molBioTestResult.setDog(molBioTestDTO.getDog());
+        molBioTestResult.setCat(molBioTestDTO.getCat());
+        molBioTestResult.setChicken(molBioTestDTO.getChicken());
+        molBioTestResult.setBuffalo(molBioTestDTO.getBuffalo());
+        molBioTestResult.setCattle(molBioTestDTO.getCattle());
+        molBioTestResult.setHorse(molBioTestDTO.getHorse());
+        molBioTestResult.setGoat(molBioTestDTO.getGoat());
+        molBioTestResult.setSheep(molBioTestDTO.getSheep());
+        molBioTestResult.setSwine(molBioTestDTO.getSwine());
+
+        // Save updated MolBioTestResult
+        return molBioRepository.save(molBioTestResult);
+    }
+}
